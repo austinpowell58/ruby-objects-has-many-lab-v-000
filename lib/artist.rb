262326b9b76023b7be9fba323ec6fe70a13a9a61
Austin Powell <austinpowell58@gmail.com>
2018-08-20 00:49:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 class Artist
-  attr_accessor :name, :song 
+  attr_accessor :name, :song , :list_of_songs
   
   def initialize(name)
     @name = name
+  end 
+  
+  def songs
+    @songs
   end 
